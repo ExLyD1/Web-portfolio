7fe3c67e31839d1d911fead7f6ef461620588d88
ExLyD1 <sashaahnij@gmail.com>
2025-08-05 19:20:08 +0300

    added new Projects Images

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
 		<!-- Skills Section -->
 		<SkillsBlock />
 
+		<!-- projects -->
 		<section
 			id="projects"
 			class="py-16 px-6 bg-gradient-to-r from-purple-900/30 to-violet-900/30 backdrop-blur-sm relative z-10"
@@ -29,7 +30,9 @@
 						filterStore.currentPage + 1 < filterStore.totalPages &&
 						!filterStore.isLoading
 					"
-					@click="filterStore.loadProjects(filterStore.currentPage + 1)"
+					@click="
+						filterStore.loadProjects(filterStore.currentPage + 1)
+					"
 					class="flex w-fit place-self-center mt-10 px-8 py-3 bg-gradient-to-r from-purple-600 to-violet-600 text-white rounded-lg hover:from-purple-500 hover:to-violet-500 transition-all duration-300 shadow-lg shadow-purple-500/30 hover:shadow-purple-500/50 transform hover:-translate-y-1 hover:scale-105 scroll-smooth cursor-pointer"
 				>
 					Завантажити ще
@@ -44,5 +47,22 @@
 
 <script lang="ts" setup>
 import { useFilterStore } from '#imports'
+import type { IProject } from '~/widgets/model/projects'
 const filterStore = useFilterStore()
+
+onMounted(() => {
+	const project: IProject = {
+		title: 'SantAvto',
+		description: 'Фриланс замовлення для сервісу обслуговування авто',
+		date: '2025-08-03',
+		tags: ['Nuxt 3', 'Tailwind'],
+		type: ['code', 'demo'],
+		difficulty: 'easy',
+		project_link: 'https://santavto.vercel.app',
+		code_link: 'https://github.com/ExLyD1/SantAvto',
+		video_link: '',
+		image: '/projects/santavto.jpg',
+	}
+	// useAlgoliaImportOne(project)
+})
 </script>
